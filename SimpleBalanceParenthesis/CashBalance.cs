caf194de4040ca533eb115fe5ef28db72f4d7e8a
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-16 20:41:39 +0530

    [Rakshit] Add: Simple Balance Program

---
+++ SimpleBalanceParenthesis/CashBalance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBalanceParenthesis
+{
+    public class CashBalance
+    {
+        public Stack stack = new Stack();
+        public string expression = "(5+6)*(7+8)/(4+3)(5+6)*(7+8)/(4+3)";
+
+        public void BalanceCheck()
+        {
+            char[] arrayOfexpression = expression.ToCharArray();
+
+            for (int i = 0; i < arrayOfexpression.Length; i++)
+            {
+                if (arrayOfexpression[i] == '(')
+                {
+                    stack.Push(arrayOfexpression[i]);
+                }
+                if (arrayOfexpression[i] == ')')
+                {
+                    if (stack.top.Equals('('))
+                    {
+                        stack.Pop();
+                    }
+                }
+
+            }
+
+            if (stack.IsEmpty().Equals(0))
+            {
+                System.Console.WriteLine(" Arithmetic Expression is Balanced");
+            }
+            else
+            {
+                System.Console.WriteLine(" Arithmetic Expression is not balanced");
+            }
+        }
+    }
+}
